d66cbb319920a39ba0772c1c87dbddae83cccdfe
Azhar <azhar.rasheed@nybbletech.com>
2018-02-27 22:54:15 +0500

    delay type is changed

--- src/android/PedoListener.java
+++ src/android/PedoListener.java
@@ -149,7 +149,8 @@ public class PedoListener extends CordovaPlugin implements SensorEventListener {
         // If found, then register as listener
         if ((list != null) && (list.size() > 0)) {
             this.mSensor = list.get(0);
-            if (this.sensorManager.registerListener(this, this.mSensor, SensorManager.SENSOR_DELAY_UI)) {
+            //if (this.sensorManager.registerListener(this, this.mSensor, SensorManager.SENSOR_DELAY_UI)) {
+			if (this.sensorManager.registerListener(this, this.mSensor, SensorManager.SENSOR_DELAY_UI)) {
                 this.setStatus(PedoListener.STARTING);
             } else {
                 this.setStatus(PedoListener.ERROR_FAILED_TO_START);
